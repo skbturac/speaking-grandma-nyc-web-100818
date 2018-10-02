7ce11749959d65d26fcb44ed6fb942b880e2a7d7
Suheyl Turac <skbsturac@live.com>
2018-10-02 15:16:40 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,6 @@
 # Write a speak_to_grandma method.
-def phrase != phrase.upcase
+def speak_to_grandma(phrase)
+  if phrase != phrase.upcase
   return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
